e6dfa3b2162b2128b76a135dbb1b144c0b9b8f3e
DashrathGelot <48720952+DashrathGelot@users.noreply.github.com>
2019-03-19 18:41:41 +0530

    celsius to feranheat convert program in c#

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Celsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string c;
+            Console.WriteLine("Hello friend enter your choice");
+            do
+            {
+                Console.WriteLine("1-Celsius To Farenheat  2-farenheat to celsius");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("enter celsius");
+                            double cel = double.Parse(Console.ReadLine());
+                            double f = (cel * 1.8) + 35;
+                            Console.WriteLine(cel+" celsius= "+f+" ferenhest");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("enter feranheat");
+                            double fe = double.Parse(Console.ReadLine());
+                            double ce = (fe-32)*0.56;
+                            Console.WriteLine(fe + " ferenheat= " + ce + " celsius");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("enter right choice");
+                            break;
+                        }
+                }
+                Console.WriteLine("you want continue (y/n)");
+                c = Console.ReadLine();
+            } while (c != "n" && c!="N");
+            //Console.ReadLine();
+        }
+    }
+}
