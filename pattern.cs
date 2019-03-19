74e79eee4f52ce9d4d083b0539c7c5344fb8cc22
DashrathGelot <48720952+DashrathGelot@users.noreply.github.com>
2019-03-19 18:51:33 +0530

    simple pattern

---
+++ pattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! this is pattern program");
+            for(int i=0;i<=5;i++ )
+            {
+                Console.WriteLine("");
+                for(int j=0;j<=5;j++)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
